b1c4aa8cdc48583650e6bfb911c90ad31335bb75
timmyu32 <35674082+timmyu32@users.noreply.github.com>
2018-09-04 21:30:17 -0400

    Add files via upload

---
+++ ghw.py
@@ -0,0 +1,72 @@
+import requests
+import time
+from lxml import etree
+
+list_of_words = [''' Adultery, 
+                fornication, 
+                uncleanness, 
+                lasciviousness, 
+                Idolatry, 
+                witchcraft, 
+                hatred, 
+                variance, 
+                emulations, 
+                wrath, 
+                strife, 
+                seditions, 
+                heresies, 
+                Envyings, 
+                murders, 
+                drunkenness, 
+                revellings''']
+
+
+for wrd in list_of_words[0].split(',')[1:-1]:
+
+    try:
+        word = wrd.replace(" ",'').lower()
+        
+
+        url = 'http://www.dictionary.com/browse/{}?s=t'.format(word)
+        
+
+        page = requests.get(url)
+        soup = page.text
+        page.close()
+
+        doc = etree.HTML(soup)
+
+        definition = doc.xpath('//span[@class="css-4x41l7 e10vl5dg6"]')[0]
+        print(word)
+        print(definition.text)
+    except:
+        word = wrd.replace(" ",'').lower()
+        print(word)
+
+        url = 'http://www.dictionary.com/misspelling?term={}&s=t'.format(word)
+        
+
+        page = requests.get(url)
+        soup = page.text
+        page.close()
+
+        doc = etree.HTML(soup)
+
+        new_word = doc.xpath('//a[@data-linkid="rtxtk5"]')
+        new_word = new_word[0].text
+        
+        url = 'http://www.dictionary.com/browse/{}?s=t'.format(new_word)
+        
+
+        page = requests.get(url)
+        soup = page.text
+        page.close()
+
+        doc = etree.HTML(soup)
+
+        definition = doc.xpath('//span[@class="css-4x41l7 e10vl5dg6"]')[0]
+        print('{} ({})'.format(word, new_word))
+        print(definition.text)
+        
+
+
